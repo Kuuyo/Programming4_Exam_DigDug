4ba4344fb210ee695acf54549e09572b036dfe02
Elias Hernes <brokenkyuu@gmail.com>
2019-05-03 15:57:37 +0200

    Set proper access specifiers to Minigin members

--- Minigin/Minigin.h
+++ Minigin/Minigin.h
@@ -8,21 +8,24 @@ namespace dae
 
 	class Minigin
 	{
+	public:
+		void Run();
+
+	protected:
+		virtual void LoadGame() const = 0;
+
+	private:
+		void Initialize();
+		void InitializeSDL();
+
+		void GameLoop();
+
+		void Cleanup();
+
 		const unsigned int m_ConstFPS = 60;
 		const float m_MsPerFrame = m_ConstFPS / 1000.f;
 		const unsigned int m_MaxUpdates = 10;
 		SDL_Window* m_pWindow{};
 		Log* m_pLog{};
-
-	public:
-		void Run();
-
-		void Initialize();
-		void InitializeSDL();
-
-		virtual void LoadGame() const = 0;
-		void GameLoop();
-
-		void Cleanup();
 	};
 }
